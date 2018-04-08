1b38f14cb0babdc4e470519e04918a0bb89a780b
mbounakhla <bounakhlamehdi@gmail.com>
2018-04-08 14:57:15 +0200

    Update Verificateur.cpp

--- Verificateur.cpp
+++ Verificateur.cpp
@@ -3,6 +3,8 @@
 #include "Hacheur.h"
 #include "Signature.h"
 
+#define HASH_SIZE 64  // taille du hash, 256 bits chaine hexadecimale
+
 
 Verificateur::Verificateur(void)
 {
@@ -10,7 +12,8 @@ Verificateur::Verificateur(void)
 }
 
 std::string chars_to_string(unsigned char* source){
-	return std::string res(source, source + 64);
+	return "";
+	//return std::string res(source, source + 64);
 }
 
 bool verify_transaction_input (TXI* txi){
@@ -28,12 +31,21 @@ bool verify_transaction (TX* tx){
 	}
 	return true;
 }
-bool verify_bloc (Bloc* b, std::string hash_bloc_precedant){
+
+bool verify_bloc (Bloc* b, std::string hash_bloc_precedant){     // sinon Bloc N-1 pour recuperer hash_bloc_precedant
 	//return (((b->previous_hash).compare(hash_bloc_precedant) != 0) && verify_transaction(b->tx1))
 	//return false;
-	return ((chars_to_string(b->previous_hash).compare(hash_bloc_precedant)) && verify_transaction(&(b->tx1)));
+	return (
+		(chars_to_string(b->previous_hash).compare(hash_bloc_precedant)) && 
+		verify_hash(b, b->hash) &&
+		verify_transaction(&(b->tx1)));
 }
 
+bool verify_hash (Bloc* b, std::string hash_bloc) {
+	for (int i=0; i<HASH_SIZE; i++)
+		b->hash[0];
+	return verifier_hachage(b->ToString(), hash_bloc);
+}
 
 Verificateur::~Verificateur(void)
 {
